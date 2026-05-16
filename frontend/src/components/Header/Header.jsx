35b23dd0d057120b417c4983ecb417559ea0fcb5
nguyen-1610 <duynguyenhuynh250@gmail.com>
2026-05-16 15:05:22 +0700

    feat(frontend): update header title for CI/CD test

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -16,7 +16,7 @@ function Header() {
         <div>
             <nav className='header bg-slate-200 flex justify-between items-center'>
                 <div className="logo w-1/4 text-center">
-                    <NavLink to="/">Todo App</NavLink>
+                    <NavLink to="/" className="font-bold text-blue-600 text-xl">Todo App (CI/CD Deployed)</NavLink>
                 </div>
                 <div className='flex justify-between'>
                     {
